6cfcf56d2f5ebc388ebcc9545dd6dec818c17e0f
MkhalilDesignfy <m.khalil@designfy.net>
2021-07-31 07:52:41 +0200

    add result of command

--- app/Enums/Commands.php
+++ app/Enums/Commands.php
@@ -9,13 +9,18 @@ class Commands extends BaseEnum
     const CONFIG = 1;
     const CACHE = 2;
     const VIEW = 3;
+    const KEY = 4;
+    const STORAGE_LINK = 5;
 
     protected static function labels(): array
     {
         return [
-            self::CONFIG => __('Config'),
-            self::CACHE  => __('Cache'),
-            self::VIEW   => __('View'),
+            self::CONFIG       => __('Config'),
+            self::CACHE        => __('Cache'),
+            self::VIEW         => __('View'),
+            self::KEY          => __('Generate Key'),
+            self::STORAGE_LINK => __('Storage Link'),
+
         ];
     }
 
@@ -28,6 +33,10 @@ class Commands extends BaseEnum
                 return 'cache:clear';
             case self::VIEW:
                 return 'view:clear';
+            case self::KEY:
+                return 'key:generate';
+            case self::STORAGE_LINK:
+                return 'storage:link';
             default:
                 return '';
         }
